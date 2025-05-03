948f68cc6bad81fb497b963fab72a3d0a8b3405a
Simon Favour Sunday <95985027+Favourlisticc@users.noreply.github.com>
2025-05-03 08:29:40 +0100

    Update schema/property.js

--- src/database/schema/properties.js
+++ src/database/schema/properties.js
@@ -2,21 +2,21 @@
 const mongoose = require('mongoose');
 
 const propertySchema = new mongoose.Schema({
-  featuredImage: { type: String, required: true },
+  featuredImage: { type: String },
   galleryImages: [{ type: String }],
-  subscriptionForm: { type: String, required: true },
-  propertyName: { type: String, required: true },
-  amount: { type: Number, required: true },
-  state: { type: String, required: true },
-  city: { type: String, required: true },
-  propertyType: { type: String, required: true },
-  propertyStatus: { type: String, required: true },
-  bedrooms: { type: Number, required: true },
-  bathrooms: { type: Number, required: true },
-  landSize: { type: Number, required: true },
-  description: { type: String, required: true },
-  commission: { type: Number, required: true },
-  indirectcommission: { type: String, required: true },
+  subscriptionForm: { type: String },
+  propertyName: { type: String },
+  amount: { type: Number },
+  state: { type: String },
+  city: { type: String },
+  propertyType: { type: String },
+  propertyStatus: { type: String },
+  bedrooms: { type: Number },
+  bathrooms: { type: Number },
+  landSize: { type: Number },
+  description: { type: String },
+  commission: { type: Number },
+  indirectcommission: { type: String },
   createdAt: { type: Date, default: Date.now }
 });
 
